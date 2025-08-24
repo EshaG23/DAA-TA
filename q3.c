dadbea7f1fa9929c3cee64b9ac6190bc99009faa
EshaG23 <ghoshesha06@gmail.com>
2025-08-24 23:20:20 +0530

    Update q3.c

--- q3.c
+++ q3.c
@@ -11,26 +11,32 @@ char emails[MAX_ACCOUNTS * MAX_EMAILS][MAX_LEN];
 char names[MAX_ACCOUNTS][MAX_LEN];
 int emailCount = 0;
 // find index of email or add if not exists
-int getEmailIndex(char *email) {
-    for (int i = 0; i < emailCount; i++) {
+int getEmailIndex(char *email) 
+{
+    for (int i = 0; i < emailCount; i++) 
+    {
         if (strcmp(emails[i], email) == 0) return i;
     }
     strcpy(emails[emailCount], email);
     return emailCount++;
 }
 // DFS to collect connected emails
-void dfs(int node, int comp[], int *size) {
+void dfs(int node, int comp[], int *size) 
+{
     visited[node] = 1;
     comp[(*size)++] = node;
-    for (int i = 0; i < emailCount; i++) {
-        if (adj[node][i] && !visited[i]) {
+    for (int i = 0; i < emailCount; i++) 
+    {
+        if (adj[node][i] && !visited[i]) 
+        {
             dfs(i, comp, size);
         }
     }
 }
 int main() {
     int N = 6;  // number of accounts
-    char accounts[6][3][MAX_LEN] = {
+    char accounts[6][3][MAX_LEN] = 
+    {
         {"Esha", "esha123@mail.com", "esha@mail.com"},
         {"Disha", "disha@mail.com", "disha123@mail.com"},
         {"Esha", "esha@mail.com", "eshaNY@mail.com"},
@@ -39,10 +45,12 @@ int main() {
         {"Esha", "esha99@mail.com", ""}
     };
     // Step 1: build graph
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++) 
+    {
         strcpy(names[i], accounts[i][0]);
         int first = -1;
-        for (int j = 1; j < 3 && accounts[i][j][0] != '\0'; j++) {
+        for (int j = 1; j < 3 && accounts[i][j][0] != '\0'; j++) 
+        {
             int idx = getEmailIndex(accounts[i][j]);
             if (first == -1) first = idx;
             adj[first][idx] = adj[idx][first] = 1; // connect
@@ -50,7 +58,8 @@ int main() {
     }
     // Step 2: DFS to find connected components
     memset(visited, 0, sizeof(visited));
-    for (int i = 0; i < emailCount; i++) {
+    for (int i = 0; i < emailCount; i++) 
+    {
         if (!visited[i]) {
             int comp[100], size = 0;
             dfs(i, comp, &size);
