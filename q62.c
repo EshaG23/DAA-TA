71c3b249d140a080c3982243cccebe9a2d4f2588
EshaG23 <ghoshesha06@gmail.com>
2025-08-24 23:18:53 +0530

    Update q62.c

--- q62.c
+++ q62.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #define INF 999999
-int main() {
+int main() 
+{
     int n;
     printf("Enter number of nodes: ");
     scanf("%d", &n);
     int cost[20][20];
     printf("Enter adjacency matrix (use %d for no edge):\n", INF);
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < n; j++) 
+        {
             scanf("%d", &cost[i][j]);
         }
     }
@@ -18,11 +20,14 @@ int main() {
     int best = direct;
     int steiner = -1;
     // check all possible intermediate nodes
-    for (int k = 0; k < n; k++) {
+    for (int k = 0; k < n; k++) 
+    {
         if (k != src && k != dest) {
-            if (cost[src][k] != INF && cost[k][dest] != INF) {
+            if (cost[src][k] != INF && cost[k][dest] != INF) 
+            {
                 int via = cost[src][k] + cost[k][dest];
-                if (via < best) {
+                if (via < best) 
+                {
                     best = via;
                     steiner = k;
                 }
@@ -31,11 +36,15 @@ int main() {
     }
     printf("Direct cost from %d -> %d = %d\n", src, dest, direct);
 
-    if (steiner != -1) {
+    if (steiner != -1) 
+    {
         printf("Better path found via node %d with cost = %d\n", steiner, best);
         printf("Node %d acts as a Steiner Point.\n", steiner);
-    } else {
+    } 
+    else 
+    {
         printf("Direct path is the best. No Steiner Point needed.\n");
     }
     return 0;
 }
+
